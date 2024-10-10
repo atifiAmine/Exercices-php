4afd1935104d8da115caf5be3927b39d8fb27e25
ATIFI AMINE <atifi_a@JEAN-ROSTAND.lan>
2024-10-10 11:05:02 +0200

    Ex4.php

---
+++ Ex4.php
@@ -0,0 +1,13 @@
+<?php
+$Argument1 = "Antoine";
+$Argument2 = "Griezman";
+function concatenation($Argument1,$Argument2){
+    return  $Argument1.' '.$Argument2;
+    
+}
+echo concatenation($Argument1,$Argument2);
+
+
+
+
+?>
